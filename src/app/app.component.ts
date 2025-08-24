86f93f1b9d37a3f0537999d344b123d56bcb6e5c
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2025-08-24 13:10:07 -0300

    Criação de serviços de usuarios e detalhes esteticos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import {MatSnackBarModule} from '@angular/material/snack-bar'
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: 
+  [
+    RouterOutlet, 
+    HeaderComponent, 
+    FooterComponent, 
+    MatSnackBarModule,
+    MatInputModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,11 +1,109 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,RouterLink, Router} from '@angular/router';
+import {UserService } from '../../services/user.service';
+import {FormsModule, NgForm} from '@angular/forms'
+import { User } from '../../../../users/users';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+
+  constructor(
+    private routerQuery: ActivatedRoute,
+    private service: UserService,
+    private router: Router,
+    private snackbar: MatSnackBar
+  ){}
+
+  isNewUser: boolean = false;
+
+  h2Text: string = "Entrar";
+  buttonText: string = "Login";
+
+  userName:string = "";
+  password:string = "";
+
+
+
+
+  ngOnInit(): void {
+    this.routerQuery.queryParams.subscribe(params => {
+      this.isNewUser = params["novo_usuario"] === "true"
+
+      if (this.isNewUser){
+        this.h2Text = "Novo Usuario"
+        this.buttonText = "Cadastrar"
+      }else{
+        this.h2Text = "Entrar"
+        this.buttonText = "Login"
+        console.log("Passou aqui")
+      }
+
+    });
+  }
+
+  authenticateUser():void{
+    let user: User = {
+      name: this.userName,
+      password: this.password
+    }
+    this.service.authenticateUser(user)
+  }
+
+  createUser():void{
+    let user: User = {
+      name: this.userName,
+      password: this.password
+    }
+
+    this.service.createUser(user).subscribe({
+      next: (response) => {
+      console.log('Usuário criado com sucesso!', response);
+      this.router.navigate(
+        [],
+        {
+          queryParams: {}
+        }
+      )
+    },
+    error: (error) => {
+      console.error('Erro ao criar usuário:', error);
+    }
+    });
+  }
+
+  submitHandler(form:NgForm): void{
+    if(this.isNewUser){
+      this.createUser();
+      form.reset()
+    }else{
+      this.authenticateUser();
+      form.reset()
+    }
+  }
+
+  SnackBarHandler(): void{
+    if(this.isNewUser){
+      this.openSnackBar('Cadastro Concluido Com Sucesso')
+    }else{
+      this.openSnackBar('Login Concluido Com Sucesso')
+    }
+  }
+
+  openSnackBar(message: string):void{
+    this.snackbar.open(message, 'Ok', {
+      duration: 5000,
+      verticalPosition: 'bottom',
+      horizontalPosition: 'center'
+
+    });
+  }
+
 
 }
