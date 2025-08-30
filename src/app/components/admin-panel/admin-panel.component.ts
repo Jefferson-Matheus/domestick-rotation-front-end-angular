5648ba38d81fb3f24a84b0db0ad28ae8076e5326
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2025-08-29 22:57:24 -0300

    Configuração para utilização de rotas

--- src/app/components/admin-panel/admin-panel.component.ts
+++ src/app/components/admin-panel/admin-panel.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {faUser, faBroomBall,faExclamationCircle} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome'
+import { RouterLink, RouterOutlet} from "@angular/router";
+
 
 @Component({
   selector: 'app-admin-panel',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterLink, RouterOutlet],
   templateUrl: './admin-panel.component.html',
   styleUrl: './admin-panel.component.css'
 })
